08bd5077b6a4c758575cefb65f88b5250850af15
huiquan zhou <714254320@qq.com>
2014-09-02 14:31:56 +0800

    The more context auto add in config.php after install web
    Signed-off-by: huiquan zhou <714254320@qq.com>

---
+++ opencart/upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/op_git/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/op_git/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/catalog/');
+define('DIR_SYSTEM', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/system/');
+define('DIR_DATABASE', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/system/database/');
+define('DIR_LANGUAGE', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/system/config/');
+define('DIR_IMAGE', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/image/');
+define('DIR_CACHE', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/download/');
+define('DIR_LOGS', 'D:\f_disk\owner\zhq\web\op_git\opencart\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'git');
+define('DB_PREFIX', 'oc_');
+?>
